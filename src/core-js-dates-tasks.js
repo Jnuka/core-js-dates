4cbcb501cf2869f6d6fd0be64e32527540658d1e
Jnuka <jnuka@mail.ru>
2025-03-11 14:09:32 +0400

    feat: add getNextFriday()

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -69,8 +69,13 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const result = new Date(date);
+  result.setDate(
+    date.getDate() +
+      (date.getDay() < 5 ? 5 - date.getDay() : 7 - (date.getDay() - 5))
+  );
+  return result;
 }
 
 /**
